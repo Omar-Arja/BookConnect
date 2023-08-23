05748340c3201943a1c1ae8650d48d0c8b5af6ad
Omar-Arja <omaralarja17@gmail.com>
2023-08-23 21:18:47 +0300

    <BE> created schemas

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,50 @@
 const mongoose = require("mongoose");
 
+const likeSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+}, {
+  timestamps: true
+})
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  genre: String,
+  review: String,
+  pic_url: String,
+
+  likes: [likeSchema]
+}, {
+  timestamps: true
+
+})
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
+    unique: true,
     required: true
   },
   password: String,
-  name: String
+  name: String,
+
+  posts: [postSchema],
+
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }],
+
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }]
+}, {
+  timestamps: true
+
 })
 
 const model = mongoose.model("User", userSchema)
