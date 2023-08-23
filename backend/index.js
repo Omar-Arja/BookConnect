ba43ed5f159c6fd3f1c1f951d85c2b63e890adbf
Omar-Arja <omaralarja17@gmail.com>
2023-08-24 00:49:07 +0300

    <BE> updated index & auth middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
-const app = express();
-const cors = require('cors');
+const app = express()
+const cors = require('cors')
+const path = require("path")
 
 require("dotenv").config()
 
@@ -8,11 +9,17 @@ const mongoDb = require("./configs/mongodb.connection");
 
 app.use(express.json())
 app.use(cors());
+
+app.use("/uploads", express.static(path.join(__dirname, "storage/uploads")));
+
 const usersRoutes = require("./routes/users.route")
 app.use("/users", usersRoutes)
 
 const authRoutes = require("./routes/auth.route")
 app.use("/auth", authRoutes)
+
+const postsRoutes = require("./routes/posts.route")
+app.use("/posts", postsRoutes)
 
 app.listen(8000, (err) => { 
   if (err) {
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,18 +1,20 @@
 const jwt = require("jsonwebtoken");
+const {User} = require("../models/user.model");
 
-const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]
+const authMiddleware = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).send({ message: 'Unauthorized' })
+  if (!token) return res.status(401).send({ message: 'Unauthorized' });
 
   try {
-    jwt.verify(token, process.env.SECRET_KEY);
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(decodedToken._id);
+    req.user = user;
     next();
-
   } catch (error) {
-    return res.status(401).send({ message: 'Unauthorized' })
+    console.error("Error during authentication:", error);
+    return res.status(401).send({ message: 'Unauthorized' });
   }
+};
 
-}
-
-module.exports = authMiddleware
+module.exports = authMiddleware;
