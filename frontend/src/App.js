a6658a1751511aa53699c6fb2f049f986ff6ac2e
Omar-Arja <omaralarja17@gmail.com>
2023-08-24 23:04:47 +0300

    <FE> initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./Pages/Auth/Login";
+import SignUpPage from "./Pages/Auth/Signup";
+import Home from "./Pages/Home/Home";
+
+function App() {
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="/discover" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default App;
