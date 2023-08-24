2ed4d88d6325772c677e01b9a3c449b74012b9f6
Omar-Arja <omaralarja17@gmail.com>
2023-08-24 23:06:00 +0300

    <FE> created Home page

---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./style.css";
+import Sidebar from "../../Components/SideBar/SideBar";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdOutlineExplore, MdPersonSearch } from "react-icons/md";
+import { FaUserAlt } from "react-icons/fa";
+
+
+const Home = () => {
+    const items = [
+        {
+            label: "Discover",
+            icon: <MdOutlineExplore />,
+        },
+        {
+            label: "My Feed",
+            icon: <FaUserAlt />,
+        },
+        {
+            label: "Find Users",
+            icon: <MdPersonSearch className="find-users-icon" />,
+        },
+        {
+            label: "Create Post",
+            icon: <AiOutlinePlus className="create-post-icon" />,
+        },
+    ];
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [books, setBooks] = useState(null);
+
+    const handleSearch = async () => {
+        
+    };
+
+    return (
+        <div className="home">
+            <Sidebar items={items} />
+            <div className="main-content">
+                { books && <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Search books..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        className="search-input"
+                    />
+                    {<BiSearchAlt2 className="search-icon" />}
+                </div>}
+                <div className="book-results">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
