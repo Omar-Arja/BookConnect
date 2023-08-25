dc02530355c13f364f8f04ae90fbf17d6bb2eab7
Omar-Arja <omaralarja17@gmail.com>
2023-08-25 04:51:13 +0300

    <FE> added loading states

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -9,80 +9,90 @@ import { MdOutlineExplore, MdPersonSearch } from "react-icons/md";
 import { FaUserAlt } from "react-icons/fa";
 import { useEffect } from "react";
 
-
 const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [books, setBooks] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [searchTerm, setSearchTerm] = useState("");
-    const [books, setBooks] = useState(null);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await sendRequest({
+          method: "GET",
+          route: "/posts/default",
+        });
+        console.log(response);
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const response = await sendRequest({
-                    method: "GET",
-                    route: "/posts/default",
-                });
-                console.log(response);
-    
-                if (response.length > 0) {
-                    setBooks(response);
-                } else {
-                    setBooks(null);
-                }
-            } catch (error) {
-                console.error("Error fetching posts:", error);
-            }
-        };
-    
-        fetchPosts();
-    }, []);
-            
-
-    const items = [
-        {
-            label: "Discover",
-            icon: <MdOutlineExplore />,
-        },
-        {
-            label: "My Feed",
-            icon: <FaUserAlt />,
-        },
-        {
-            label: "Find Users",
-            icon: <MdPersonSearch className="find-users-icon" />,
-        },
-        {
-            label: "Create Post",
-            icon: <AiOutlinePlus className="create-post-icon" />,
-        },
-    ];
-
-    const handleSearch = async () => {
-        
+        if (response.length > 0) {
+          setBooks(response);
+        } else {
+          setBooks(null);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+        setLoading(false);
+      }
     };
 
-    return (
-        <div className="home">
-            <Sidebar items={items} />
-            <div className="main-content">
-                { books && <div className="search-bar">
-                    <input
-                        type="text"
-                        placeholder="Search books..."
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                        className="search-input"
-                    />
-                    {<BiSearchAlt2 className="search-icon" />}
-                </div>}
-                <div className="book-results">
-                    {books?.map((book) => (
-                        <BookCard key={book._id} book={book} />
-                    ))}
-                </div>
-            </div>
+    fetchPosts();
+  }, []);
+
+  const items = [
+    {
+      label: "Discover",
+      icon: <MdOutlineExplore />,
+    },
+    {
+      label: "My Feed",
+      icon: <FaUserAlt />,
+    },
+    {
+      label: "Find Users",
+      icon: <MdPersonSearch className="find-users-icon" />,
+    },
+    {
+      label: "Create Post",
+      icon: <AiOutlinePlus className="create-post-icon" />,
+    },
+  ];
+
+  return (
+    <div className="home">
+      <Sidebar items={items} />
+      <div className="main-content">
+        {books && (
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search books..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="search-input"
+            />
+            {<BiSearchAlt2 className="search-icon" />}
+          </div>
+        )}
+        {!books && (
+          <div className="empty-state">
+            {loading ? (
+              <h1>Loading...</h1>
+            ) : (
+              <>
+                <h2>No Posts Yet</h2>
+                <button className="button">Share your favorite book</button>
+              </>
+            )}
+          </div>
+        )}
+        <div className="book-results">
+          {books?.map((book) => (
+            <BookCard key={book._id} book={book} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- frontend/src/Pages/MyFeed/MyFeed.jsx
+++ frontend/src/Pages/MyFeed/MyFeed.jsx
@@ -1,29 +1,45 @@
 import "./style.css";
-import { useState, useEffect } from "react";
-import { sendRequest } from "../../config/request";
+import React, { useState } from "react";
 import Sidebar from "../../Components/SideBar/SideBar";
+import BookCard from "../../Components/BookCard/BookCard";
+import { sendRequest } from "../../config/request";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdOutlineExplore, MdPersonSearch } from "react-icons/md";
 import { FaUserAlt } from "react-icons/fa";
+import { useEffect } from "react";
 
 
-const MyFeed = () => {
+const Home = () => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+    const [books, setBooks] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const response = await sendRequest({ method: "GET", route: "/posts/" });
-            console.log(response);
-
-            if (response.length > 0) {
-                setBooks(response);
-            } else {
-                setBooks(null);
+            setLoading(true);
+            try {
+                const response = await sendRequest({
+                    method: "GET",
+                    route: "/posts",
+                });
+                console.log(response);
+    
+                if (response.length > 0) {
+                    setBooks(response);
+                } else {
+                    setBooks(null);
+                }
+            } catch (error) {
+                console.error("Error fetching posts:", error);
             }
+            setLoading(false);
         };
-
+    
         fetchPosts();
     }, []);
+            
 
     const items = [
         {
@@ -44,15 +60,12 @@ const MyFeed = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = useState("");
-    const [books, setBooks] = useState(null);
-
     const handleSearch = async () => {
         
     };
 
     return (
-        <div className="MyFeed">
+        <div className="home">
             <Sidebar items={items} />
             <div className="main-content">
                 { books && <div className="search-bar">
@@ -65,15 +78,27 @@ const MyFeed = () => {
                     />
                     {<BiSearchAlt2 className="search-icon" />}
                 </div>}
-                { !books && <div className="empty-state">
-                    <h2>Nothing to see here</h2>
-                            </div>}
-                <div className="book-results">
+                {!books && (
+                    <div className="empty-state">
+                        {loading ? (
+                        <h1>Loading...</h1>
+                        ) : (
+                        <>
+                            <h2>No Posts Yet</h2>
+                            <button className="button">Share your favorite book</button>
+                        </>
+                        )}
+                    </div>
+                    )}
 
+                <div className="book-results">
+                    {books?.map((book) => (
+                        <BookCard key={book._id} book={book} />
+                    ))}
                 </div>
             </div>
         </div>
     );
 };
 
-export default MyFeed;
+export default Home;
